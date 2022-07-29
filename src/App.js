b075b0aa400304ee3aabcf63c890c75dd6736e9f
ykuruvilla <yasha.kuruvilla@gmail.com>
2022-07-29 10:13:08 +0100

    create form component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-function App() {
+import { Route, Switch } from "react-router-dom";
+import Header from "./components/Header/Header";
+
+import "./App.scss";
+import Home from "./components/Home/Home";
+import Form from "./components/Form/Form";
+import Results from "./components/Results";
+
+const App = () => {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/questionnaire" component={Form} />
+        <Route exact path="/results" component={Results} />
+      </Switch>
+    </>
   );
-}
+};
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "./Header.scss";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      Header
+      <h1>Test</h1>
+    </div>
+  );
 };
 
 export default Header;
