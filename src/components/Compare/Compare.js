225eb8d8ffb18cea804558f7e863f73e0cc098d7
ykuruvilla <yasha.kuruvilla@gmail.com>
2022-07-29 13:41:04 +0100

    style

--- src/components/Compare/Compare.js
+++ src/components/Compare/Compare.js
@@ -4,13 +4,23 @@ import car1 from "../../assets/images/car1.png";
 import car2 from "../../assets/images/car2.png";
 import car3 from "../../assets/images/car3.png";
 import "./Compare.scss";
+import breadcrumbs from "../../assets/images/Breadcrumb _ 02.png";
 
 const Compare = () => {
   return (
     <div className="compare__cards-wrapper">
+      <img className="breadcrumbs" src={breadcrumbs} alt="" />
       <div className="compare__cards">
-        <Card img={car1} make="Vauxhall" model="Mokka E" years="2" fuelImg="" />
         <Card
+          price="£30.45k"
+          img={car1}
+          make="Vauxhall"
+          model="Mokka E"
+          years="2"
+          fuelImg=""
+        />
+        <Card
+          price="£40k"
           isMiddle={true}
           img={car2}
           make="Volkswagen"
@@ -18,7 +28,14 @@ const Compare = () => {
           years="2.1"
           fuelImg=""
         />
-        <Card img={car3} make="Tesla" model="Model 3" years="3" fuelImg="" />
+        <Card
+          price="£60k"
+          img={car3}
+          make="Tesla"
+          model="Model 3"
+          years="3"
+          fuelImg=""
+        />
       </div>
     </div>
   );
