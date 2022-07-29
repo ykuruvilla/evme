4e0e80f4cfa49f88a8fca1b093cec05b33266130
Georgia Plitsch <georgia.plitsch@outlook.com>
2022-07-29 14:13:41 +0100

    home complete

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,8 @@
 import "./Home.scss";
+import breadcrumbs from "../../assets/images/breadcrumb all blue .png";
+import socials from "../../assets/images/social-icons .png";
+import down from "../../assets/images/vectorchevron.png";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -10,23 +14,66 @@ const Home = () => {
             <h1 className="home__hero-header"> easier for you.</h1>
           </div>
           <div>
-            <h2 className="home__hero-subheader">
+            <p className="home__hero-subheader">
               Find your perfect EV in 3 simple steps
-            </h2>
+            </p>
           </div>
+        </div>
+        <div className="home__down">
+          <img className="home__down-img" src={down}></img>
         </div>
       </section>
 
       <section className="home__steps">
-        <article className="home__steps-container">
-          <div>
-            <div className="home__steps-img"></div>
-            <p className="home__steps-text">
-              Put in your vehicle details of your existing car or one that you
-              would like to compare
-            </p>
+        <div className="home__steps-container">
+          <article className="home__steps-step">
+            <div className="home__steps-card">
+              <div className="home__steps-img"></div>
+              <p className="home__steps-text--bold">Details</p>
+              <p className="home__steps-text">
+                Put in your vehicle details of your existing car or one that you
+                would like to compare
+              </p>
+            </div>
+          </article>
+
+          <article className="home__steps-step">
+            <div className="home__steps-card">
+              <div className="home__steps-img2"></div>
+              <p className="home__steps-text--bold">Compare</p>
+              <p className="home__steps-text">
+                Compare vehicles by their carbon offset amount and choose which
+                will save you the most money in the future
+              </p>
+            </div>
+          </article>
+
+          <article className="home__steps-step">
+            <div className="home__steps-card">
+              <div className="home__steps-img3"></div>
+              <p className="home__steps-text--bold">Purchase</p>
+              <p className="home__steps-text">
+                Once chosen click the chosen manufacturers link and be elligible
+                for a discount!
+              </p>
+            </div>
+          </article>
+        </div>
+        <div className="home__steps-breadcrumbs--container">
+          <img className="home__steps-breadcrumbs" src={breadcrumbs}></img>
+          <div className="home__steps-breadcrumbs--text">
+            <p>Step 1</p>
+            <p>Step 2</p>
+            <p>Step 3</p>
           </div>
-        </article>
+        </div>
+
+        <Link to="/questionnaire">
+          <button className="home__button">
+            <h3 className>Let's go</h3>
+          </button>
+        </Link>
+        <img className="home__socials" src={socials}></img>
       </section>
     </div>
   );
