98ebdd862795c52c01de9f90a0cad4a65cd53b6a
ykuruvilla <yasha.kuruvilla@gmail.com>
2022-07-29 11:25:01 +0100

    style header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "./Header.scss";
+import logo from "../../assets/logo/logo.png";
 
 const Header = () => {
   return (
-    <div>
-      Header
-      <h1>Test</h1>
-    </div>
+    <header className="header">
+      <div className="header__logo-container">
+        <a href="/">
+          <img className="header__logo" src={logo} alt="logo" />
+        </a>
+      </div>
+      <div className="header__navigation-container">
+        <span className="header__navigation-list-item">about us</span>
+        <span className="header__navigation-list-item header__navigation-list-item--center">
+          profile
+        </span>
+        <span className="header__navigation-list-item">compare</span>
+      </div>
+    </header>
   );
 };
 
